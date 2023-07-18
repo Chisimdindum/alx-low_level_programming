cacc20f42d9ceb08b869f95bd89541451cab84ab
Chisimdindum <chisimdimartins@gmail.com>
2023-07-18 15:32:16 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
-*main - main entry point
-*return : always 0 (success)
-*/
+ * main - function for holding code
+ *
+ * Return: always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
