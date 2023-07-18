b54d053d128d0b9adede35a1e635c4e9efdf6217
Chisimdindum <chisimdimartins@gmail.com>
2023-07-18 15:52:28 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - displays a series of words
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
